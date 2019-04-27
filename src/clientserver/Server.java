cebaf5bc1a6c8774db7d95a1a0051e490f379d86
Břetislav Král <bretakral@gmail.com>
2019-04-27 13:34:30 +0200

    little update

--- src/clientserver/Server.java
+++ src/clientserver/Server.java
@@ -13,6 +13,27 @@ public class Server extends Thread {
 	
 	public void run() {
 		while(true) {
+			try {
+			System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
+			Socket server = serverSocket.accept();
+			
+			System.out.println("Just connected to " + server.getRemoteSocketAddress());
+			DataInputStream in = new DataInputStream(server.getInputStream());
+			
+			System.out.println(in.readUTF());
+			DataOutputStream out = new DataOutputStream(server.getOutputStream());
+			out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");
+			
+			server.close();
+			} catch (SocketTimeoutException s) {
+				System.out.println("Socket timed out!");
+				break;
+			} catch (IOException e) {
+				e.printStackTrace();
+				break;
+			}
+			
+			
 			
 		}
 	}
